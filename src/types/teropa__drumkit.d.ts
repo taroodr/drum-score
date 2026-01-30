9a36442436af6dd4bdf4ac7bd68c365795590399
taroodr <n.kotaro.0101@gmail.com>
2026-01-30 11:19:34 +0900

    Add types for @teropa/drumkit

---
+++ src/types/teropa__drumkit.d.ts
@@ -0,0 +1,11 @@
+declare module "@teropa/drumkit" {
+  export const kick: string;
+  export const snare: string;
+  export const hatClosed: string;
+  export const hatOpen: string;
+  export const tomLow: string;
+  export const tomMid: string;
+  export const tomHigh: string;
+  export const ride: string;
+  export const crash: string;
+}
